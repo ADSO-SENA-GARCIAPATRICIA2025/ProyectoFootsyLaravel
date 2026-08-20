f72ff910a96329860dace17553d512de95a657a8
Jeanny Patricia Garcia <patgarcia.abafg@gmail.com>
2026-08-20 21:45:37 +0200

    Api rest Productos

--- app/Filament/Resources/Categorias/Tables/CategoriasTable.php
+++ app/Filament/Resources/Categorias/Tables/CategoriasTable.php
@@ -24,7 +24,7 @@ class CategoriasTable
                     ->sortable(),
             ])
             ->filters([
-                //
+                
             ])
             ->recordActions([
                 EditAction::make(),
